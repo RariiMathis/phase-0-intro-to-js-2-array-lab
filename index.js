fdd5cc1fa322c30d8c155889446b54cffc44447d
Rarii Mathis <juniormathis82@gmail.com>
2023-08-17 14:07:30 -0400

    not done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+// Array
+cats = ["Milo", "Otis", "Garfield"];
+function cats(){
+    cats.length 
+}
+function destructivelyAppendCat(){
+cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Bob");
+}
+function appendCat(){
+    return cats.splice(0, 3, "Broom");
+}
+function prependCat(){
+    return prependCat(0, 4)
+}
+function removeLastCat(){
+    return removeLastCat.splice(4, )
+}
+function removeFirstCat(){
+    return removeFirstCat.splice()
+}
